cef377887b1d2a19a0518da74a5012e2c4edd2ba
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-06-06 18:22:40 -0300

    Properties
    Criado método que recupera os dados preenchidos no arquivo properties

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ Criado utilizando java com interface Swing/Desktop, e banco de dados MySql.
 
 ##
 ### Configurações de Ambiente
-SO: Pop!_OS/Windows 10<br>
+SO: Pop!_OS (Linux) / Windows 10<br>
 JDK: 17.0.6<br>
-Apache/2.4.54<br>
 MySQL: 8.0.32 Community
+
+### Instalação/Configuração
+Java: <a href="https://www.youtube.com/watch?v=QekeJBShCy4" target="_blank">Windows</a> / <a href="https://www.youtube.com/watch?v=Kn87QF1mEcY" target="_blank">Linux</a><br>
+MySQL: <a href="https://www.youtube.com/watch?v=DKMrVxtJyUk" target="_blank">Windows</a> / <a href="https://youtu.be/CBK7c1xp-zI" target="_blank">Linux</a><br>
+NetBeans: <a href="https://www.youtube.com/watch?v=dmnerOpFj1E" target="_blank">Windows</a> / <a href="https://www.youtube.com/watch?v=SiPJcbrW9K8" target="_blank">Linux</a>
+
+### Acesso ao Banco de dados
+Para fazer o acesso ao banco de dados é necessário informar o usuário e senha do banco de dados configurado em seu ambiente, através do arquivo "db_modelo.properties" e renomear este para "db.properties".
--- src/conexoes/ConexaoMySql.java
+++ src/conexoes/ConexaoMySql.java
@@ -4,16 +4,16 @@ package conexoes;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author Diego Barbosa da Silva
  */
 public class ConexaoMySql {
@@ -24,36 +24,42 @@ public class ConexaoMySql {
     private Statement statement;
     private ResultSet resultSet;
 
+    public ConexaoMySql() {
+    }
 
-    private String servidor = "localhost";
-    private String nomeDoBanco = "bdvendasprojeto";
-    private String usuario = "root";
-    private String senha = "privada3";
-    
-    public ConexaoMySql(){}
-    
-    public ConexaoMySql(String pServidor, String pNomeDoBanco, String pUsuario, String pSenha){
-        this.servidor = pServidor;
-        this.nomeDoBanco = pNomeDoBanco;
-        this.usuario = pUsuario;
-        this.senha = pSenha;
+    /**
+     * Retorna os dados de acesso configurados no properties.
+     *
+     * @return prop
+     */
+    private static Properties loadProperties() {
+        try {
+            Properties prop = new Properties();
+            String caminho = "/db.properties";
+            prop.load(ConexaoMySql.class.getResourceAsStream(caminho));
+            return prop;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Fueda");
+        }
+        return null;
     }
 
     /**
      * Abre uma conexao com o banco
+     *
      * @return Connection
      */
-    public Connection conectar(){
+    public Connection conectar() {
         try {
             //Driver do MySQL
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-
-            //Local do banco, nome do banco, usuario e senha
-            String url = "jdbc:mysql://localhost:3306/bdvendasprojeto?serverTimezone=UTC";
-            this.setCon((Connection) DriverManager.getConnection(url, usuario, senha));
+            Properties properties = loadProperties();
+            final String urlBanco = properties.getProperty("banco.url");
+            final String userBanco = properties.getProperty("banco.usuario");
+            final String passwordBanco = properties.getProperty("banco.senha");
+            this.setCon((Connection) DriverManager.getConnection(urlBanco, userBanco, passwordBanco));
             this.status = true;
-        }
-        catch(ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e){
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
         return this.getCon();
@@ -61,154 +67,118 @@ public class ConexaoMySql {
 
     /**
      * Executa consultas SQL
+     *
      * @param pSQL
      * @return int
      */
-    public boolean executarSQL(String pSQL){
+    public boolean executarSQL(String pSQL) {
         try {
             //createStatement de con para criar o Statement
             this.setStatement(getCon().createStatement());
 
             // Definido o Statement, executamos a query no banco de dados
             this.setResultSet(getStatement().executeQuery(pSQL));
-            
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
             return false;
         }
         return true;
     }
-    
-    public boolean executarUpdateDeleteSQL(String pSQL){
+
+    public boolean executarUpdateDeleteSQL(String pSQL) {
         try {
-            
             //createStatement de con para criar o Statement
             this.setStatement(getCon().createStatement());
-
             // Definido o Statement, executamos a query no banco de dados
             getStatement().executeUpdate(pSQL);
-            
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
             return false;
         }
         return true;
     }
-    
+
     /**
      * Executa insert SQL
+     *
      * @param pSQL
      * @return boolean
      */
-    public int insertSQL(String pSQL){
+    public int insertSQL(String pSQL) {
         int status = 0;
         try {
             //createStatement de con para criar o Statement
             this.setStatement(getCon().createStatement());
-
             // Definido o Statement, executamos a query no banco de dados
             this.getStatement().executeUpdate(pSQL);
-            
             //consulta o ultimo id inserido
             this.setResultSet(this.getStatement().executeQuery("SELECT last_insert_id();"));
-            
             //recupera o ultimo id inserido
-            while(this.resultSet.next()){
+            while (this.resultSet.next()) {
                 status = this.resultSet.getInt(1);
             }
-            
             //retorna o ultimo id inserido
             return status;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
             return status;
         }
     }
 
     /**
      * encerra a conexão corrente
+     *
      * @return boolean
      */
-    public boolean fecharConexao(){
-       try {
-           if((this.getResultSet() != null) && (this.statement != null)){
-               this.getResultSet().close();
-               this.statement.close();
-           }
-           this.getCon().close();
-           return true;
-       } catch(SQLException e) {
-           JOptionPane.showMessageDialog(null, e.getMessage());
-       }
-       return false;
+    public boolean fecharConexao() {
+        try {
+            if ((this.getResultSet() != null) && (this.statement != null)) {
+                this.getResultSet().close();
+                this.statement.close();
+            }
+            this.getCon().close();
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return false;
     }
-    
+
     public boolean isStatus() {
         return this.status;
     }
-    
+
     public String getMensagem() {
         return mensagem;
     }
-    
+
     public Statement getStatement() {
         return statement;
     }
-    
+
     public ResultSet getResultSet() {
         return resultSet;
     }
-    
+
     public void setMensagem(String mensagem) {
         this.mensagem = mensagem;
     }
-    
+
     public Connection getCon() {
         return con;
     }
-    
+
     public void setCon(Connection con) {
         this.con = con;
     }
-    
+
     public void setStatement(Statement statement) {
         this.statement = statement;
     }
-    
+
     public void setResultSet(ResultSet resultSet) {
         this.resultSet = resultSet;
     }
     
-    public String getServidor() {
-        return servidor;
-    }
-    
-    public void setServidor(String servidor) {
-        this.servidor = servidor;
-    }
-    
-    public String getNomeDoBanco() {
-        return nomeDoBanco;
-    }
-    
-    public void setNomeDoBanco(String nomeDoBanco) {
-        this.nomeDoBanco = nomeDoBanco;
-    }
-    
-    public String getUsuario() {
-        return usuario;
-    }
-    
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-    
-    public String getSenha() {
-        return senha;
-    }
-    
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
 }
